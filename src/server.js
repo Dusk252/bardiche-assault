deef614794c899a9db2fcf9a59c1f8f2f3d79f73
Dusk252 <Dusk252@users.noreply.github.com>
2022-10-14 04:41:31 +0300

    [update] define server port

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const server = express();
+const port = process.env.PORT || 3001;
 
 server.all('/', (_, res) => {
     res.send('Result: [OK]');
 });
 
 function keepAlive(client) {
-    server.listen(3000, async () => {
+    server.listen(port, async () => {
         await client.mongoClient.connect();
         const date = new Date();
         console.log(`Server is ready! | ${date.toLocaleDateString()} ${date.toLocaleTimeString()}`);
