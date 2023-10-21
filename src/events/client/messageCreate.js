f0dae1336fbab771e08a3ba70b43a9ad2cf5807b
Dusk252 <Dusk252@users.noreply.github.com>
2023-10-21 17:16:32 +0300

    [add] twitter links fix

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -2,6 +2,8 @@ module.exports = {
     name: 'messageCreate',
     execute: async (message) => {
         if (message.content.startsWith('Bardiche'))
-        message.reply('``YES SIR!``');
+            await message.reply('``YES SIR!``');
+        else if (/^http(?:s)?:\/\/(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/.test(message.content))
+            await message.reply({ content: message.content.replace('e', 'p'), allowedMentions: { repliedUser: false } });
     },
 };
